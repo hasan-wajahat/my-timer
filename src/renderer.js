be7e0c9065fd2f3c15d6eb36a688b8059312d7a0
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-09-21 17:07:45 +0500

    Adds React

--- src/renderer.js
+++ src/renderer.js
@@ -26,6 +26,10 @@
  * ```
  */
 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(<App />, document.getElementById("root"));
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
