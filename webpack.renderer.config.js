99b8cf132c77ec6a6a19e7f632d15a7f2cdc112a
Hasan Wajahat <hasan_wajahat@hotmail.com>
2019-09-21 16:20:51 +0500

    Adds babel

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -2,7 +2,23 @@ const rules = require('./webpack.rules');
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{
+    loader: 'style-loader'
+  }, {
+    loader: 'css-loader'
+  }],
+}, {
+  test: /\.m?js$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env', '@babel/preset-react']
+    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
 });
 
 module.exports = {
